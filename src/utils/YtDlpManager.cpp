96a1a606eefdd2a7450f0627aa681df93a8d09a5
MohamedSubarashi <mohamed.subarashi@gmail.com>
2026-09-23 18:06:29 +0100

    fix: cross-platform yt-dlp pause/resume (signal.h) and correct Arch source tarball

--- src/utils/YtDlpManager.cpp
+++ src/utils/YtDlpManager.cpp
@@ -14,6 +14,7 @@
 #include <QJsonObject>
 #include <QJsonArray>
 #include <QRegularExpression>
+#include <signal.h>
 
 YtDlpManager::YtDlpManager() : isDownloading(false), nam(new QNetworkAccessManager(this)), activeReply(nullptr) {}
 
